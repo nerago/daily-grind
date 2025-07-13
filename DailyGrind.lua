c6a324b2d0d74261a5db9220e32d137660a190a4
Nicholas Hardy <nicholas.h.hardy@gmail.com>
2025-07-13 12:03:09 +1000

    colouring etc

--- DailyGrind.lua
+++ DailyGrind.lua
@@ -16,17 +16,18 @@ function addon:OpenFrame()
 		old_CloseSpecialWindows = CloseSpecialWindows
 		CloseSpecialWindows = function()
 			local found = old_CloseSpecialWindows()
-			if self.frame then
-				self.frame:Hide()
+			if addon.frame then
+				addon.frame:Hide()
 				return true
 			end
 			return found
 		end
 	end
 	
-	local frame = self.frame
+	local frame = addon.frame
 	if not frame then
 		frame = self:CreateMainFrame()
+		addon.frame = frame
 	end
 	
 	frame:Show()
@@ -46,6 +47,7 @@ function addon:CreateMainFrame()
 	
 	local tree = gui:Create("TreeGroup")
 	tree:SetTree(data)
+	tree:SetTreeWidth(300, true)
 	frame:AddChild(tree)
 end
 
@@ -61,6 +63,18 @@ local function getZoneName(zoneId)
 	return zoneName
 end
 
+local function textUnavailable(text)
+	return "|cff808080"..text..FONT_COLOR_CODE_CLOSE
+end
+
+local function textAvailable(text)
+	return "|cffC0C000"..text..FONT_COLOR_CODE_CLOSE
+end
+
+local function textComplete(text)
+	return "|cff20E020"..text..FONT_COLOR_CODE_CLOSE
+end
+
 function addon:BuildData()
 	local dataByZone = {};
 	local zoneNameList = {};
@@ -70,16 +84,24 @@ function addon:BuildData()
 		local questInfo = questieDb.GetQuest(questId)
 		if questInfo then
 			zoneName = getZoneName(questInfo.zoneOrSort)
-			local fullName = questInfo.name .. " (" .. questId .. ")"
-			local doable, complete = questieDb.IsDoable(questId), questieDb.IsComplete(questId)
+			local name = questInfo.name
+			local text = name .. " (" .. questId .. ")"
+			local status = 0
+			local doable, complete = questieDb.IsDoable(questId), C_QuestLog.IsQuestFlaggedCompleted(questId)
 			if complete then
-				item = { text = fullName, value = questId, disabled = true }
+				text = textComplete(text)
+				status = 1
 			elseif doable then
-				item = { text = fullName, value = questId }
+				text = textAvailable(text)
+				status = 2
+			else
+				text = textUnavailable(text)
+				status = 3
 			end
+			item = { text = text, name = name, value = questId, status = status }
 		else
 			zoneName = "Unknown"
-			item = { text = "Unknown quest " .. questId, value = questId }
+			item = { text = "Unknown quest " .. questId, name = "Unknown quest", value = questId, status = 3 }
 		end
 		
 		if item then
@@ -98,7 +120,11 @@ function addon:BuildData()
 	local resultData = {}
 	for _, zoneName in ipairs(zoneNameList) do
 		local zoneQuests = dataByZone[zoneName]
-		table.sort(zoneQuests, function(a,b) return a.text < b.text end)
+		table.sort(zoneQuests, function(a,b) 
+			if a.status < b.status then return true
+			elseif a.status >= b.status then return false
+			else return a.text < b.text end
+		end)
 		
 		local header = { text = zoneName, value = zoneName, children = zoneQuests }
 		table.insert(resultData, header)
